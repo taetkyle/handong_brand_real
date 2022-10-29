8416324458f3265142b49bc9416eeabe53b9152b
(Kyungtae Park) <(taetkyle37@gmail.com)>
2022-10-30 01:41:44 +0900

    First commit

---
+++ android/app/src/main/kotlin/com/example/his_brand_cupang/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.his_brand_cupang
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/searchpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../main.dart';
+
+class searchpage extends StatefulWidget {
+  const searchpage({super.key});
+
+  @override
+  State<searchpage> createState() => _searchpageState();
+}
+
+class _searchpageState extends State<searchpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
